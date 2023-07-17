a94565dd74595601fe892614bbf71afb65af03cd
Runit Mhatre <mhatrerunit777@gmail.com>
2023-07-17 22:30:15 +0530

    Message

---
+++ frontend/src/errors/http_errors.ts
@@ -0,0 +1,19 @@
+class HttpError extends Error {
+    constructor(message?: string) {
+        super(message);
+        this.name = this.constructor.name;
+    }    
+}
+
+/**
+ *  Status Code 401
+ */
+
+export class UnauthorizedError extends HttpError {} 
+
+/**
+ *  Status Code 409
+ */
+
+export class ConFlictError extends HttpError {}
+
