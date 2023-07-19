217ed76094908d6d389fc23f6a6bc9d05d1c082c
Runit Mhatre <mhatrerunit777@gmail.com>
2023-07-19 14:13:24 +0530

    added vercel config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,11 @@ import mongoose from "mongoose";
 
 const port = env.PORT;
 
-mongoose.connect(env.MONGO_CONNECTION_STRING)
-    .then(() => {
-        console.log("Mongoose connected");
-        app.listen(port, () => {
-            console.log("Server running on port: " + port);
-        });
-    })
-    .catch(console.error);
+
+    app.listen(port,async () => {
+        console.log(`Connecting...`);
+       await mongoose.connect(env.MONGO_CONNECTION_STRING);
+       console.log('Connected to database');
+    });
+
+    export default app;
